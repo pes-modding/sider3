934708c9ac34473476ea5f3eff07d48f8be72119
Anton Jouline <juce66@gmail.com>
2017-01-26 21:49:36 -0800

    shared memory segments do not work under Wine. trying without them

--- sider.h
+++ sider.h
@@ -2,7 +2,7 @@
 #define SIDER_H
 
 #include <string>
-#include "shared.h"
+//#include "shared.h"
 
 using namespace std;
 
@@ -11,7 +11,7 @@ __declspec(dllexport) void unsetHook();
 __declspec(dllexport) void log_(const wchar_t *format, ...);
 __declspec(dllexport) void start_log_(const wchar_t *format, ...);
 __declspec(dllexport) void get_module_version(HMODULE, wstring&);
-__declspec(dllexport) struct WSTR_INFO *get_wi();
-__declspec(dllexport) int *get_dll_mapping_option();
+//__declspec(dllexport) struct WSTR_INFO *get_wi();
+//__declspec(dllexport) int *get_dll_mapping_option();
 
 #endif
