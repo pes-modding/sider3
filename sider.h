6ec46b526127a33f385133ff322de7022507ce38
Anton Jouline <juce66@gmail.com>
2017-01-28 01:40:33 -0800

    minor clean-up

--- sider.h
+++ sider.h
@@ -2,16 +2,15 @@
 #define SIDER_H
 
 #include <string>
-//#include "shared.h"
 
 using namespace std;
+
+#define SIDER_FM L"Local\\sider-3"
 
 __declspec(dllexport) void setHook();
 __declspec(dllexport) void unsetHook();
 __declspec(dllexport) void log_(const wchar_t *format, ...);
 __declspec(dllexport) void start_log_(const wchar_t *format, ...);
 __declspec(dllexport) void get_module_version(HMODULE, wstring&);
-//__declspec(dllexport) struct WSTR_INFO *get_wi();
-//__declspec(dllexport) int *get_dll_mapping_option();
 
 #endif
