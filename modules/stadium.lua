b0dfe18208a06f935f700762f55b6c7de075f5c7
Anton Jouline <juce66@gmail.com>
2017-03-22 14:35:11 -0700

    module tweaks

--- modules/stadium.lua
+++ modules/stadium.lua
@@ -1,15 +1,21 @@
--- Stadium switcher: for English Super Cup
+-- Stadium switcher example:
+-- for English Super Cup, go to Camp Nou.
 
 local m = {}
 
 function m.set_stadium(ctx, options)
     if ctx.tournament_id == 101 then
         log("Switching to Camp Nou on a rainy winter day")
-        -- English Super Cup
+
         options.stadium = 2   -- Camp Nou
         options.timeofday = 0 -- Day
         options.weather = 1   -- Rain
         options.season = 1    -- Winter
+
+        -- return the options table to indicate
+        -- that this change is final. This stops processing
+        -- of the event and modules further down the list
+        -- will not receive this event.
         return options
     end
 end
