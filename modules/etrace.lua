53e158bdfb234a69614cffebf3f5395819f0cfe7
Anton Jouline <juce66@gmail.com>
2017-03-22 18:16:41 -0700

    etrace.lua update

--- modules/etrace.lua
+++ modules/etrace.lua
@@ -3,39 +3,46 @@
 
 local m = {}
 
-local function log_context(ctx)
+local function t2s(t)
     local parts = {}
-    for k,v in pairs(ctx) do
+    for k,v in pairs(t) do
         parts[#parts + 1] = string.format("%s=%s", k, v)
     end
-    log(string.format("ctx: %s", table.concat(parts,",")))
+    table.sort(parts) -- sort alphabetically
+    return string.format("{%s}", table.concat(parts,", "))
 end
 
 function m.set_home(ctx, id)
     log(string.format("home team: %d", id))
-    log_context(ctx)
+    log(string.format("ctx: %s", t2s(ctx)))
 end
 
 function m.set_away(ctx, id)
     log(string.format("away team: %d", id))
-    log_context(ctx)
+    log(string.format("ctx: %s", t2s(ctx)))
 end
 
 function m.set_match_time(ctx, minutes)
-    log(string.format("num minutes: %d", minutes))
-    log_context(ctx)
+    log(string.format("match time: %d", minutes))
+    log(string.format("ctx: %s", t2s(ctx)))
 end
 
 function m.set_tid(ctx, tid)
     log(string.format("tournament_id: %d", tid))
-    log_context(ctx)
+    log(string.format("ctx: %s", t2s(ctx)))
+end
+
+function m.set_stadium(ctx, options)
+    log(string.format("stadium options: %s", t2s(options)))
+    log(string.format("ctx: %s", t2s(ctx)))
 end
 
 function m.init(ctx)
-   ctx.register("set_home_team", m.set_home); 
-   ctx.register("set_away_team", m.set_away); 
-   ctx.register("set_tournament_id", m.set_tid); 
-   ctx.register("set_match_time", m.set_match_time); 
+   ctx.register("set_home_team", m.set_home)
+   ctx.register("set_away_team", m.set_away)
+   ctx.register("set_tournament_id", m.set_tid)
+   ctx.register("set_match_time", m.set_match_time)
+   ctx.register("set_stadium", m.set_stadium)
 end
 
 return m
