d24679a138bdf8c6afe5873511d6903777650a8d
Anton Jouline <juce66@gmail.com>
2017-04-06 14:20:47 -0700

    updates for example modules; small tweak for scripting doc

--- modules/gameversion.lua
+++ modules/gameversion.lua
@@ -17,7 +17,7 @@ end
 function m.init(ctx)
     local addr = memory.search(pattern, 0x400000, 0x4000000)
     if addr then
-        -- version should be write after the pattern
+        -- version should be right after the pattern
         addr = addr + #pattern
         log(string.format("found version info at address: %08x", addr))
         local ver = memory.read(addr, 32)
