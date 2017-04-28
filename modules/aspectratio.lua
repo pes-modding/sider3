2762c1ae2be00bba1df5d8208536cbcdc834afd7
Anton Jouline <juce66@gmail.com>
2017-04-27 23:41:47 -0700

    faster aspect-ratio cmd

--- modules/aspectratio.lua
+++ modules/aspectratio.lua
@@ -1,11 +1,11 @@
 -- set aspect ratio
 -- For PES2017.exe 1.04 and 1.04.01 only
 
-local aspect_ratio = 1.4
+local aspect_ratio = 1.6
 
 function init(ctx)
     memory.write(0x1f51c1c, memory.pack("f", aspect_ratio))
-    memory.write(0x383f2bf, "\x90\x90\x90\x90\xeb")
+    memory.write(0x383f2bf, "\xeb\x5b\x90\x90")
 end
 
 return { init = init }
