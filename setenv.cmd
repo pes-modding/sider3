d1175331696fce11a61298143f46e3e5ef7534d6
Anton Jouline <juce66@gmail.com>
2017-06-09 23:59:44 -0700

    setenv update

--- setenv.cmd
+++ setenv.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Setting kitserver compile environment
-@call "c:\program files\microsoft visual studio 12.0\vc\vcvarsall.bat"
+@call "c:\program files\microsoft visual studio 14.0\vc\vcvarsall.bat"
 echo Environment set
 
